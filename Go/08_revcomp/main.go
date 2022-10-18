d74bec7abe1c86196bfb716714ba938517a8608a
Jose Balanza Martinez <braulio.balanza@gmail.com>
2022-10-18 05:16:07 +0000

    Go code now fetches fasta.txt url and executes.

--- Go/08_revcomp/main.go
+++ Go/08_revcomp/main.go
@@ -8,69 +8,79 @@
 package main
 
 import (
-   "bufio"
-   "os"
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 const lineSize = 60
 
 var complement = [256]uint8{
-   'A': 'T', 'a': 'T',
-   'C': 'G', 'c': 'G',
-   'G': 'C', 'g': 'C',
-   'T': 'A', 't': 'A',
-   'U': 'A', 'u': 'A',
-   'M': 'K', 'm': 'K',
-   'R': 'Y', 'r': 'Y',
-   'W': 'W', 'w': 'W',
-   'S': 'S', 's': 'S',
-   'Y': 'R', 'y': 'R',
-   'K': 'M', 'k': 'M',
-   'V': 'B', 'v': 'B',
-   'H': 'D', 'h': 'D',
-   'D': 'H', 'd': 'H',
-   'B': 'V', 'b': 'V',
-   'N': 'N', 'n': 'N',
+	'A': 'T', 'a': 'T',
+	'C': 'G', 'c': 'G',
+	'G': 'C', 'g': 'C',
+	'T': 'A', 't': 'A',
+	'U': 'A', 'u': 'A',
+	'M': 'K', 'm': 'K',
+	'R': 'Y', 'r': 'Y',
+	'W': 'W', 'w': 'W',
+	'S': 'S', 's': 'S',
+	'Y': 'R', 'y': 'R',
+	'K': 'M', 'k': 'M',
+	'V': 'B', 'v': 'B',
+	'H': 'D', 'h': 'D',
+	'D': 'H', 'd': 'H',
+	'B': 'V', 'b': 'V',
+	'N': 'N', 'n': 'N',
 }
 
 func main() {
-   in := bufio.NewReaderSize(os.Stdin, 1<<18)
-   buf := make([]byte, 1<<20)
-   line, err := in.ReadSlice('\n')
-   for err == nil {
-      os.Stdout.Write(line)
 
-      // Accumulate reversed complement in buf[w:]
-      nchar := 0
-      w := len(buf)
-      for {
-         line, err = in.ReadSlice('\n')
-         if err != nil || line[0] == '>' {
-            break
-         }
-         line = line[0 : len(line)-1]
-         nchar += len(line)
-         if len(line)+nchar/lineSize+128 >= w {
-            nbuf := make([]byte, len(buf)*5)
-            copy(nbuf[len(nbuf)-len(buf):], buf)
-            w += len(nbuf) - len(buf)
-            buf = nbuf
-         }
+	// TODO: change url to server domain
+	resp, err := http.Get("https://localhost:2028/assets/fasta.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-         for i, c := range line {
-            buf[w-i-1] = complement[c]
-         }
-         w -= len(line)
-      }
+	in := bufio.NewReaderSize(resp.Body, 1<<18)
+	buf := make([]byte, 1<<20)
+	line, err := in.ReadSlice('\n')
+	for err == nil {
+		os.Stdout.Write(line)
 
-      // Copy down to beginning of buffer, inserting newlines.
-      // The loop left room for the newlines and 128 bytes of padding.
-      i := 0
-      for j := w; j < len(buf); j += lineSize {
-         i += copy(buf[i:i+lineSize], buf[j:])
-         buf[i] = '\n'
-         i++
-      }
-      os.Stdout.Write(buf[0:i])
-   }
+		// Accumulate reversed complement in buf[w:]
+		nchar := 0
+		w := len(buf)
+		for {
+			line, err = in.ReadSlice('\n')
+			if err != nil || line[0] == '>' {
+				break
+			}
+			line = line[0 : len(line)-1]
+			nchar += len(line)
+			if len(line)+nchar/lineSize+128 >= w {
+				nbuf := make([]byte, len(buf)*5)
+				copy(nbuf[len(nbuf)-len(buf):], buf)
+				w += len(nbuf) - len(buf)
+				buf = nbuf
+			}
+
+			for i, c := range line {
+				buf[w-i-1] = complement[c]
+			}
+			w -= len(line)
+		}
+
+		// Copy down to beginning of buffer, inserting newlines.
+		// The loop left room for the newlines and 128 bytes of padding.
+		i := 0
+		for j := w; j < len(buf); j += lineSize {
+			i += copy(buf[i:i+lineSize], buf[j:])
+			buf[i] = '\n'
+			i++
+		}
+		os.Stdout.Write(buf[0:i])
+	}
 }
