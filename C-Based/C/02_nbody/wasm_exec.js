2aa599a55aee5f6f226f84752ee144ec3ae5f7dc
Jose Balanza Martinez <braulio.balanza@gmail.com>
2022-10-28 00:02:00 +0000

    Test function

--- C-Based/C/02_nbody/wasm_exec.js
+++ C-Based/C/02_nbody/wasm_exec.js
@@ -1,7 +1,8 @@
 import {stopRun} from "../../../utils/utils.js"
-import wasm from "./pkg/emscripten.js"
+// import wasm from "./pkg/emscripten.js"
 (async function main() {
-    const wasm_module = await wasm();
-    wasm_module._run()
+    // const wasm_module = await wasm();
+    // wasm_module._run()
+    await new Promise(resolve => setTimeout(resolve, 5000));
     await stopRun({})
 })();
